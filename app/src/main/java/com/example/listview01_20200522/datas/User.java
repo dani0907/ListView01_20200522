d4e6f2115c9521c22dc8478d6cdebf675bfc5e65
dy696786 <dy696786@gmail.com>
2020-05-22 10:39:34 +0900

    User 클래스 추가  - getter / setter (private변수)  - constructor (생성) 추가  - Alt + Insert 단축키로 자동 작성

---
+++ app/src/main/java/com/example/listview01_20200522/datas/User.java
@@ -0,0 +1,30 @@
+package com.example.listview01_20200522.datas;
+
+public class User {
+    private String name;
+    private String address;
+
+//    Alt + Insert
+
+
+    public User(String name, String address) {
+        this.name = name;
+        this.address = address;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+}
